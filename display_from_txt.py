b0b21ce40327af42b484f8fb1b56199064ef6637
AaronYosefCerdon <aaronyosefcerdon123@gmail.com>
2024-11-20 22:11:47 +0800

    Initial Pseudocode Steps

---
+++ display_from_txt.py
@@ -0,0 +1,7 @@
+#Define a variable for the information needed.
+
+#Use read and write to display the information of the name chosen.
+
+#Print the information.
+
+#Display error message when no information is found.
