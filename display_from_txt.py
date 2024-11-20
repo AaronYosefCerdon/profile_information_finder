91935849b6120b93274368e591070696322ab58a
AaronYosefCerdon <aaronyosefcerdon123@gmail.com>
2024-11-20 22:21:23 +0800

    Asking for a full name

--- display_from_txt.py
+++ display_from_txt.py
@@ -1,7 +1,10 @@
 #Define a variable for the information needed.
+identify_fullname = input("Please enter a full name to find information: ")
 
 #Use read and write to display the information of the name chosen.
-
+with open("./profile_list.txt","r") as profile_finder:
+	information = profile_finder.read( )
+  
 #Print the information.
 
 #Display error message when no information is found.
