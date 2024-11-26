7851d87d36509458f489e07df83aa8488a1bf765
AaronYosefCerdon <aaronyosefcerdon123@gmail.com>
2024-11-26 20:37:09 +0800

    Adding while loop to apply repeating entries.

--- display_from_txt.py
+++ display_from_txt.py
@@ -1,12 +1,13 @@
-#Define a variable for the information needed.
-identify_fullname = input("Please enter a full name to find information: ")
-#Declare a variable to track if the input is found in the txt file.
-found = False
-
-#Use read and write to display the information of the name chosen.
-with open("./profile_list.txt","r") as profile_finder:
-	information = profile_finder.read( )
-	lines = information.split("\n\n" )  
+# Use a while loop to find the information and ask for entries repeatedly.
+while True:
+    # Define a variable for the information needed.
+    identify_full_name = input("Please enter a full name to find information: ")
+    found = False
+    
+    # Use read and write to display the information of the name chosen.
+    with open("./profile_list.txt", "r") as profile_finder:
+        information = profile_finder.read( )
+        lines = information.split("\n\n" )  
         # Use a for loop to find the needed name.
         for line in lines:
             if identify_full_name in line:
@@ -15,7 +16,8 @@ with open("./profile_list.txt","r") as profile_finder:
                 print(line.strip( ))
                 found = True
                 break
-# Use an if statement to display an error message when no input is found.
+    
+    # Use an if statement to display an error message when no input is found.
     if not found:
         print(f"No corresponding profile found for {identify_full_name}.")
     
@@ -25,6 +27,4 @@ with open("./profile_list.txt","r") as profile_finder:
     # Use an if statement to end the search.
     if another_find.lower() == "no":
         print("Thank you for using the profile finder.")
-        
-		    
-
+        break
